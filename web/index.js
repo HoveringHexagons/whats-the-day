cce679a398370062f8428ee3492f2262bd2b7164
HoveringHexagons <133187793+HoveringHexagons@users.noreply.github.com>
2023-10-22 13:15:26 -0500

    Updated index.js to catch errors

--- web/index.js
+++ web/index.js
@@ -2,19 +2,26 @@ const h1 = document.querySelector("h1")
 const p = document.querySelector("p")
 const api = fetch('https://worldtimeapi.org/api/timezone/America/Detroit').then(response => response.json())
 const dayOfWeek = [
+  "sunday",
   "monday",
   "tuesday",
   "wednesday",
   "thursday",
   "friday",
   "saturday",
-  "sunday"
 ]
 const fetchTheDay = () => {
   api.then(data => {
+    if (data.error) {
+      h1.textContent = `Failed to get :(`
+      p.textContent =`
+      This might be an issue with your connection, or the server is down.`
+    }
+    else {
      h1.textContent = `
-     It is ${dayOfWeek[data.day_of_week -1]}`
+     It is ${dayOfWeek[data.day_of_week]}`
      p.textContent = ``
+    }
 })
 }
 fetchTheDay()
